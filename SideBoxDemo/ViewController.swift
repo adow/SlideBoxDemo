d29e4b61fa2c061c6520a587862169f80eec6244
adow <reynoldqin@gmail.com>
2015-11-13 10:31:50 +0800

    Initial Commit

---
+++ SideBoxDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SideBoxDemo
+//
+//  Created by 秦 道平 on 15/11/13.
+//  Copyright © 2015年 秦 道平. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
